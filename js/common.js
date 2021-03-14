5b0c30ed6fce40b696ce0e0b3a4af7853a05a792
sollan0506 <sollan0506@naver.com>
2021-03-14 19:05:01 +0900

    이용안내 서브페이지 desktop css 작업
    이용안내 서브페이지 desktop css 작업

--- js/common.js
+++ js/common.js
@@ -38,20 +38,29 @@ $(function(){
 
 
     // sub_nav hover
-    $(".contents_wrap .sub_nav .container > ul > li:nth-child(2)").hover(
+    $(".contents_wrap .sub_nav .container > ul > li:nth-child(2), .contents_wrap .sub_nav .container > ul > li:nth-child(3)").hover(
         function () {
             $(this).children("ul").stop().fadeIn(300);
         }, function () {
             $(this).children("ul").stop().fadeOut(300);
         }
     );
-    $(".contents_wrap .sub_nav .container > ul > li:nth-child(3)").hover(
-        function () {
-            $(this).children("ul").stop().fadeIn(300);
-        }, function () {
-            $(this).children("ul").stop().fadeOut(300);
+
+
+    // sub_nav 스크롤시 fixed
+    $(window).on("scroll", function(e){
+        var scrolled = $(window).scrollTop();
+        var headerHeight = $("header").height();
+
+        if(width > 768){ //desktop
+            // 스크롤이 header보다 아래로 내려가면 sub_nav 고정
+            if(scrolled > headerHeight) {
+                $(".contents_wrap .sub_nav").addClass("on");
+            } else {
+                $(".contents_wrap .sub_nav").removeClass("on");
+            }
         }
-    );
+    });
     
     
 
