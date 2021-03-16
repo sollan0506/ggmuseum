ee8d29bc8a175ac4f315145298b26e947746154c
sollan0506 <sollan0506@naver.com>
2021-03-16 21:33:24 +0900

    header 구조 수정중
    header 구조 수정중

--- js/common.js
+++ js/common.js
@@ -1,27 +1,5 @@
 $(function(){
 
-    var width = window.innerWidth;
-
-    if(width > 768){ //desktop
-        $("nav .gnb > li").hover(
-            function () {
-                // over
-                $(this).find("ul").css("display", "block").animate({"opacity": "1"}, 300);
-            }, function () {
-                // out
-                $(this).find("ul").animate({"opacity": "0"}, 300).css("display", "none");
-            }
-        );
-    } else {
-        // 모바일 메뉴 슬라이드 토글
-        $("nav .gnb > li > a").click(function (e) { 
-            e.preventDefault();
-            $(this).toggleClass("on");
-            $(this).parent().children("ul").slideToggle();
-            $("nav > div").toggleClass("on");
-        });
-    }
-
     // 햄버거버튼 토글
     $(".gnb_all_btn > a").click(function (e) { 
         $(".gnb_all").toggleClass("on");
@@ -31,8 +9,8 @@ $(function(){
     $(".gnb_mob_btn > a").click(function (e) {
         $(this).children("span").toggleClass("on");
         $("header").toggleClass("on");
-        $("nav").slideToggle();
-        $(".submenu").toggleClass("on");
+        // $("nav").slideToggle();
+        // $(".submenu").toggleClass("on");
         e.preventDefault();
     });
 
@@ -61,13 +39,34 @@ $(function(){
     });
     
     $(window).on("load resize", function(e){
-        var innerWidth = window.innerWidth;
+        var width = window.innerWidth;
 
-        if(innerWidth > 768){ //desktop
+        if(width > 768){ //desktop
+            // console.log("desktop");
+            $("nav .gnb > li").hover(
+                function () {
+                    // over
+                    $(this).find("ul").css("display", "block").animate({"opacity": "1"}, 300);
+                }, function () {
+                    // out
+                    $(this).find("ul").animate({"opacity": "0"}, 300).css("display", "none");
+                }
+            );
+            
             // 데스크탑에서 sub_nav 보여주기
             $(".contents_wrap .sub_nav.exhibition").css("display", "block");
             $("header").css("border-bottom", "0");
         } else { //mobile
+            // console.log("mobile");
+            // 모바일 메뉴 슬라이드 토글
+            // $("nav .gnb > li > a").click(function (e) { 
+            //     e.preventDefault();
+            //     $(this).toggleClass("on");
+            //     $("nav .gnb > li > ul").not($(this)).slideUp();
+            //     $(this).parent().children("ul").slideDown();
+            //     $("nav > div").toggleClass("on");
+            // });
+
             // 모바일에서 sub_nav 없는것들은 보여주지 않기
             $(".contents_wrap .sub_nav.exhibition").css("display", "none");
             $(".contents_wrap .contents.exhibition").css("padding-top", "0");
