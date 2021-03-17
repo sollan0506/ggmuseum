0945cc3b2adc4e2d540e56b762183f88fa92fd7c
sollan0506 <sollan0506@naver.com>
2021-03-17 19:55:30 +0900

    header 구조 수정 완료
    header 구조 수정 완료

--- js/common.js
+++ js/common.js
@@ -9,8 +9,7 @@ $(function(){
     $(".gnb_mob_btn > a").click(function (e) {
         $(this).children("span").toggleClass("on");
         $("header").toggleClass("on");
-        // $("nav").slideToggle();
-        // $(".submenu").toggleClass("on");
+        $(".sitemap").slideToggle();
         e.preventDefault();
     });
 
@@ -43,6 +42,7 @@ $(function(){
 
         if(width > 768){ //desktop
             // console.log("desktop");
+            
             $("nav .gnb > li").hover(
                 function () {
                     // over
@@ -52,20 +52,47 @@ $(function(){
                     $(this).find("ul").animate({"opacity": "0"}, 300).css("display", "none");
                 }
             );
+
+            // 모바일 메뉴가 열려 있는 상태로 데스크탑 크기로 resize되었을 때 모바일 메뉴 숨기기
+            $(".gnb_mob_btn > a").children("span").removeClass("on");
+            $("header").removeClass("on");
+            $(".sitemap").hide();
             
             // 데스크탑에서 sub_nav 보여주기
             $(".contents_wrap .sub_nav.exhibition").css("display", "block");
             $("header").css("border-bottom", "0");
         } else { //mobile
             // console.log("mobile");
+
             // 모바일 메뉴 슬라이드 토글
-            // $("nav .gnb > li > a").click(function (e) { 
-            //     e.preventDefault();
-            //     $(this).toggleClass("on");
-            //     $("nav .gnb > li > ul").not($(this)).slideUp();
-            //     $(this).parent().children("ul").slideDown();
-            //     $("nav > div").toggleClass("on");
-            // });
+            var liCount = $(".sitemap_gnb > ul > li").length; // li의 개수를 저장
+            var mGnbSts = []; // li의 상태를 저장하는 배열(0: 닫힘, 1: 열림)
+            for( var i = 0; i < liCount; i++ ){
+                mGnbSts.push(0); // 배열의 끝에 0을 추가
+            }
+
+            $(".sitemap_gnb .gnb_tit").click(function(e){
+                e.preventDefault();
+
+                var index = $(this).parent("li").index(); // 클릭한 li가 몇번째인지 index를 저장
+                if(mGnbSts[index] === 0){ // 배열의 index번째가 0이라면 (클릭한 li가 닫혀있다면)
+                    $(this).parent('li').siblings('li').find(".gnb_subtits").slideUp(200);
+                    $(this).parent('li').siblings('li').find(".gnb_tit").removeClass('on');
+                    $(this).addClass("on");
+                    $(this).next(".gnb_subtits").slideDown(300);
+
+                    mGnbSts = [];
+                    for( var i = 0; i < liCount; i++ ){
+                        mGnbSts.push(0); // 전부 닫힌 상태로 초기화
+                    }
+                    mGnbSts[index] = 1; // 클릭한 li번째는 열린 상태로 저장
+                } else if( mGnbSts[index] === 1 ){ // 배열의 index번째가 1이라면 (클릭한 li가 열려있다면)
+                    $(this).removeClass("on");
+                    $(this).next(".gnb_subtits").slideUp(300);
+                    
+                    mGnbSts[index] = 0; // 클릭한 li번째를 닫힌 상태로 저장
+                }
+            });
 
             // 모바일에서 sub_nav 없는것들은 보여주지 않기
             $(".contents_wrap .sub_nav.exhibition").css("display", "none");
